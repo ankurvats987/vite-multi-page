74eada0739087eb0c00badfb04fb783a387c9a8a
ankur <rockingjohnny2004@gmail.com>
2025-01-30 15:35:45 +0530

    adding dist

---
+++ dist/assets/about-legacy-BNCKlpdV.js
@@ -0,0 +1,1 @@
+System.register(["./main-legacy-5B6wJ2bv.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){document.addEventListener("DOMContentLoaded",(()=>{console.log("About page loaded"),document.querySelector("main").classList.add("fade-in")}))}}}));
---
+++ dist/assets/main-6PsWyfFL.js
@@ -0,0 +1,1 @@
+import"./main-BBAoEA1N.js";function t(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}document.addEventListener("DOMContentLoaded",()=>{console.log("Main page loaded")});export{t as __vite_legacy_guard};
